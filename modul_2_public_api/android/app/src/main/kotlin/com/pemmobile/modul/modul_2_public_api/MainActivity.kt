634af34af5a7e2dfa8ccbe95ab7b99db69d64448
Ahmad Naufal <naufalopal002@gmail.com>
2024-08-08 17:03:45 +0800

    build: downgrade version to 3.16.9

--- modul_2_public_api/android/app/src/main/kotlin/com/pemmobile/modul/modul_2_public_api/MainActivity.kt
+++ modul_2_public_api/android/app/src/main/kotlin/com/pemmobile/modul/modul_2_public_api/MainActivity.kt
@@ -2,4 +2,5 @@ package com.pemmobile.modul.modul_2_public_api
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+}
