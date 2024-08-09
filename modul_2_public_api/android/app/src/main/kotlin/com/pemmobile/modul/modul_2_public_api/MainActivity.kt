80a43f0e616bbb0e1bc24b80dccbde07f4c53a64
Ahmad Naufal <naufalopal002@gmail.com>
2024-08-09 19:25:36 +0800

    build: modul 4 implementasi kamera init project

--- modul_2_public_api/android/app/src/main/kotlin/com/pemmobile/modul/modul_2_public_api/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.pemmobile.modul.modul_2_public_api
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ modul_4_implementasi_kamera/android/app/src/main/kotlin/com/pemmobile/modul/modul_4_implementasi_kamera/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.pemmobile.modul.modul_4_implementasi_kamera
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
