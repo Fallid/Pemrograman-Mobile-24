c455118047942c297a6d8f30e48f1dc1121a8e44
Ahmad Naufal <naufalopal002@gmail.com>
2024-08-10 10:54:27 +0800

    feat: Implementasi kamera untuk foto dan video

--- modul_4_implementasi_kamera/lib/app/modules/home/controllers/home_controller.dart
+++ modul_4_implementasi_kamera/lib/app/modules/home/controllers/home_controller.dart
@@ -1,23 +1,122 @@
+import 'dart:io';
+
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:video_player/video_player.dart';
 
 class HomeController extends GetxController {
-  //TODO: Implement HomeController
+  final ImagePicker _picker = ImagePicker(); //object image picker
+  final box = GetStorage(); //get storage variable
 
-  final count = 0.obs;
+  var selectedImagePath = ''.obs; //variable untuk menyimpan path image
+  var isImageLoading = false.obs; //variable untuk loading state
+
+  var selectedVideoPath = ''.obs; //variable untuk menyimpan path video
+  var isVideoPlaying = false.obs; //variable untuk pause and play state
+  VideoPlayerController? videoPlayerController; 
+
   @override
   void onInit() {
     super.onInit();
+    _loadStoredData();
   }
 
   @override
   void onReady() {
     super.onReady();
+    _loadStoredData();
   }
 
   @override
   void onClose() {
+    videoPlayerController?.dispose();
     super.onClose();
   }
 
-  void increment() => count.value++;
+  //Function Future untuk menggunakakn kamera atau galeri
+  Future<void> pickImage(ImageSource source) async {
+    try {
+      isImageLoading.value = true;
+      final XFile? pickedFile = await _picker.pickImage(source: source);
+      if (pickedFile != null) {
+        selectedImagePath.value = pickedFile.path;
+        box.write(
+            'imagePath', pickedFile.path); //menyimpan image path ke get_storage
+      } else {
+        print('No image selected.');
+      }
+    } catch (e) {
+      print('Error picking image: $e');
+    } finally {
+      isImageLoading.value = false;
+    }
+  }
+
+  Future<void> pickVideo(ImageSource source) async {
+    try {
+      isImageLoading.value = true;
+      final XFile? pickedFile = await _picker.pickVideo(source: source);
+      if (pickedFile != null) {
+        selectedVideoPath.value = pickedFile.path;
+        box.write('videoPath', pickedFile.path);
+
+        // Initialize VideoPlayerController
+        videoPlayerController =
+            VideoPlayerController.file(File(pickedFile.path))
+              ..initialize().then((_) {
+                // Ensure the first frame is shown
+                videoPlayerController!.play();
+                isVideoPlaying.value = true; // Update status
+                update(); // Notify UI
+              });
+      } else {
+        print('No video selected.');
+      }
+    } catch (e) {
+      print('Error picking video: $e');
+    } finally {
+      isImageLoading.value = false;
+    }
+  }
+
+  void _loadStoredData() {
+    selectedImagePath.value = box.read('imagePath') ?? '';
+    selectedVideoPath.value = box.read('videoPath') ?? '';
+
+    if (selectedVideoPath.value.isNotEmpty) {
+      videoPlayerController =
+          VideoPlayerController.file(File(selectedVideoPath.value))
+            ..initialize().then((_) {
+              videoPlayerController!.play();
+              // isVideoPlaying.value = true; // Update status
+              // update(); // Notify UI
+            });
+    }
+  }
+
+  void play() {
+    videoPlayerController?.play();
+    isVideoPlaying.value = true; // Update status
+    update(); // Notify UI
+  }
+
+  void pause() {
+    videoPlayerController?.pause();
+    isVideoPlaying.value = false; // Update status
+    update(); // Notify UI
+  }
+
+  void togglePlayPause() {
+    if (videoPlayerController != null) {
+      if (videoPlayerController!.value.isPlaying) {
+        videoPlayerController!.pause();
+        isVideoPlaying.value = false; // Update status
+      } else {
+        videoPlayerController!.play();
+        isVideoPlaying.value = true; // Update status
+      }
+      update(); // Notify UI
+    }
+  }
 }
--- modul_4_implementasi_kamera/lib/app/modules/home/views/home_view.dart
+++ modul_4_implementasi_kamera/lib/app/modules/home/views/home_view.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:video_player/video_player.dart';
 
 import '../controllers/home_controller.dart';
 
@@ -10,13 +14,92 @@ class HomeView extends GetView<HomeController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
+        title: const Text('Image Picker'), elevation: 4,
       ),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              
+              // User Interface untuk menampilkan, memilih, dan mendapatkan foto menggunakan kamera atau galeri
+              SizedBox(
+                height: Get.height / 2.32,
+                width: Get.width * 0.7,
+                child: Obx(() {
+                  return controller.isImageLoading.value
+                      ? const CircularProgressIndicator()
+                      : controller.selectedImagePath.value == ''
+                          ? const Text('No image selected')
+                          : ClipRRect(borderRadius: BorderRadius.circular(20),
+                              child: Image.file(
+                                  File(controller.selectedImagePath.value),fit:BoxFit.cover ),
+                            );
+                }),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () => controller.pickImage(ImageSource.camera),
+                child: const Text('Pick Image from Camera'),
+              ),
+              ElevatedButton(
+                onPressed: () => controller.pickImage(ImageSource.gallery),
+                child: const Text('Pick Image from Gallery'),
+              ),
+              const SizedBox(height: 20),
+              const Divider(
+                color: Colors.grey,
+              ),
+
+              // User interface untuk mendapatkan, memilih, dan menampilkan video dari kamera atau galeri
+              SizedBox(
+                height: Get.height / 2.32,
+                width: Get.width * 0.7,
+                child: Obx(() {
+                  if (controller.selectedVideoPath.value.isNotEmpty) {
+                    return Card(
+                      child: Column(
+                        children: [
+                          AspectRatio(
+                            aspectRatio: 1,
+                            child:
+                                VideoPlayer(controller.videoPlayerController!),
+                          ),
+                          VideoProgressIndicator(
+                            controller.videoPlayerController!,
+                            allowScrubbing: true,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              IconButton(
+                                icon: Icon(
+                                  controller.isVideoPlaying.isTrue
+                                      ? Icons.play_arrow
+                                      : Icons.pause,
+                                ),
+                                onPressed: controller.togglePlayPause,
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    );
+                  } else {
+                    return const Text('No video selected');
+                  }
+                }),
+              ),
+              ElevatedButton(
+                onPressed: () => controller.pickVideo(ImageSource.camera),
+                child: const Text('Pick Video from Camera'),
+              ),
+              ElevatedButton(
+                onPressed: () => controller.pickVideo(ImageSource.gallery),
+                child: const Text('Pick Video from Gallery'),
+              ),
+            ],
+          ),
         ),
       ),
     );
