b848f766e89221e9f30e6780b805fb0baff9af53
Ahmad Naufal <naufalopal002@gmail.com>
2024-08-19 13:31:57 +0800

    feat: modul 6 asynchronous handling

---
+++ lib/network_controller.dart
@@ -0,0 +1,43 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class NetworkController extends GetxController {
+  final Connectivity _connectivity = Connectivity();
+
+  @override 
+  void onInit() {
+    super.onInit();
+    // Mendengarkan perubahan konektivitas
+    _connectivity.onConnectivityChanged.listen((connectivityResults) {
+      // Jika connectivityResults adalah List<ConnectivityResult>, kita ambil hasil pertama
+      _updateConnectionStatus(connectivityResults.first);
+    });
+  }
+
+  void _updateConnectionStatus(ConnectivityResult connectivityResult) {
+    if (connectivityResult == ConnectivityResult.none) {
+      // Menampilkan snackbar jika tidak ada koneksi internet
+      Get.rawSnackbar(
+        messageText: const Text(
+          'PLEASE CONNECT TO THE INTERNET',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 14,
+          ),
+        ),
+        isDismissible: false,
+        duration: const Duration(days: 1),
+        backgroundColor: Colors.red[400]!,
+        icon: const Icon(Icons.wifi_off, color: Colors.white, size: 35),
+        margin: EdgeInsets.zero,
+        snackStyle: SnackStyle.GROUNDED,
+      );
+    } else {
+      // Menutup snackbar jika koneksi internet ada
+      if (Get.isSnackbarOpen) {
+        Get.closeCurrentSnackbar();
+      }
+    }
+  }
+}
